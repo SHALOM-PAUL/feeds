4daf17e4d78c28e5fef564fe570d3edf2834084b
shalom11 <shalom@codewave.com>
2022-06-21 20:23:16 +0530

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5e91c5f586f3ce1a99bccdb7ef6460aa",
-"index.html": "2f729d3cc6b060f63cfac464d870f22d",
-"/": "2f729d3cc6b060f63cfac464d870f22d",
-"main.dart.js": "8c22998c1f4d37deeb697b4e3f396477",
+"index.html": "9b7fddf29f107a2c82f698604b88225d",
+"/": "9b7fddf29f107a2c82f698604b88225d",
+"main.dart.js": "4e95e09c96b071e6a8386dc11159c38b",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
